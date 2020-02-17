fe27787e002e13316721fef36691533e7252a2b0
王泽 <wangze@wangzedeMacBook-Pro.local>
2020-02-18 00:07:27 +0800

    引入zrender画图

--- WZCustomPageConfig.js
+++ WZCustomPageConfig.js
@@ -14,6 +14,18 @@
 	var methods = {
 
 	}
-	
-	init();
-}
+	var zr = zrender.init(document.getElementById('demo1'));
+	var circle = new zrender.Circle({
+	    shape: {
+	        cx: 150,
+	        cy: 50,
+	        r: 40
+	    },
+	    style: {
+	        fill: 'none',
+	        stroke: '#F00'
+	    }
+	});
+	zr.add(circle);
+	console.log(123)
+}()
