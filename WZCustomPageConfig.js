0fbc0ccb1dc8dc6afab3b2d83685fc137b73db67
王泽 <wangze@wangzedeMacBook-Pro.local>
2020-02-25 16:36:42 +0800

    tijian

--- WZCustomPageConfig.js
+++ WZCustomPageConfig.js
@@ -1,31 +1,51 @@
 ! function() {
-	var pageCustom = function() {
-		
-	};
-	var page = {
-		
-	};
-	var data = {
+	var pageCustom = {
 
 	};
-	var render = {
-
-	};
-	var methods = {
-
-	}
 	var zr = zrender.init(document.getElementById('demo1'));
 	var circle = new zrender.Circle({
-	    shape: {
-	        cx: 150,
-	        cy: 50,
-	        r: 40
-	    },
-	    style: {
-	        fill: 'none',
-	        stroke: '#F00'
-	    }
+		shape: {
+			cx: 50,
+			cy: 50,
+			r: 30
+		},
+		style: {
+			fill: 'transparent',
+			stroke: '#F00'
+		}
+	});
+	circle.on("click", function() {
+		console.log("circle点击事件")
 	});
 	zr.add(circle);
-	console.log(123)
+	var circle1 = new zrender.Circle({
+		shape: {
+			cx: 210,
+			cy: 50,
+			r: 40
+		},
+		style: {
+			fill: 'none',
+			stroke: '#F00'
+		}
+	});
+	zr.add(circle1);
+	
+	var rect = new zrender.Rect({
+		shape: {
+			x: 0,
+			y: 100,
+			width: 100,
+			height: 100
+		},
+		style: {
+			stroke: '#ffc8aa'
+		},
+		position: [10, 10]
+
+	});
+	rect.on('click', function() {
+		console.log('单击了这个矩形');
+	});
+	zr.add(rect);
 }()
