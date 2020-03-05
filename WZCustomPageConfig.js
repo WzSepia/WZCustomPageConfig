5e4ab298e7bd675ed378cff2ed3b97c88fa7f7b4
Sepia <wz45855283@163.com>
2020-03-05 12:45:55 +0800

    添加图片，完善操作模版，添加画布生成，文本图片区域生成功能

--- WZCustomPageConfig.js
+++ WZCustomPageConfig.js
@@ -3,6 +3,16 @@
 	依赖：zrender
 	作者：wangze
 	版本：pageCustom -V 1.0
+	
+	禁止鼠标右键：oncontextmenu="return false"；
+	禁止选择：onselectstart="return false"；
+	禁止拖放：ondragstart="return false"；
+	禁止拷贝：oncopy=document.selection.empty() 。
+	禁止复制：oncopy = "return false"；
+	禁止保存：<noscript><iframe src="*.htm"></iframe></noscript>，放在head里面。
+	禁止粘贴：<input type=text onpaste="return false">
+	禁止剪贴：oncut = "return false"；
+	关闭输入法：<input style="ime-mode:disabled">
 */
 
 //自定义表格方法集：pageCustom
@@ -11,15 +21,20 @@ var pageCustom = {
 	 * 图形元素集合
 	 * */
 	eles: {},
+	//配置保存的数据集合
+	custom_page_data: {
+		text: {}, //文本元素集合
+		imgs: {}, //图片元素集合
+		rect: {} //区域图形元素
+	},
 	/**
 	 * 自定义图形初始化
 	 * @describing init
 	 * @param el,opts
 	 * @author wangze
-	 * @version v1.0
 	 * @updatetime 2020-02-26
 	 */
-	init: function(el, options) {
+	init: function(el, options, w, h) {
 		this.e = document.getElementById(el);
 		this.opts = options;
 		var defaultOpt = this.getDefaultOptions();
@@ -27,16 +42,20 @@ var pageCustom = {
 			if (!this.opts[key]) {
 				this.opts[key] = defaultOpt[key];
 			}
-			if (this.e) this.zr = zrender.init(this.e);
+			if (this.e) {
+				this.e.style.width = w + "px";
+				this.e.style.height = h + "px";
+				this.zr = zrender.init(this.e);
+				this.oncontextmenu(this.e);
+			}
 		}
-		console.log(this.zr);
+		if (options && options.length > 0) this.customPageRender(options);
 	},
 	/**
-	 * 添加图形
-	 * @describing init
-	 * @param el,opts
+	 * 添加图形区域
+	 * @describing addRect
+	 * @param name
 	 * @author wangze
-	 * @version v1.0
 	 * @updatetime 2020-02-26
 	 */
 	addRect: function(name) {
@@ -54,8 +73,8 @@ var pageCustom = {
 			position: [10, 10]
 
 		});
-		this.eles[name].on('click', function() {
-			console.log('单击了这个矩形');
+		this.eles[name].on('click', function(e) {
+			console.log('单击了这个矩形', e);
 		});
 		this.zr.add(this.eles[name]);
 	},
@@ -64,7 +83,6 @@ var pageCustom = {
 	 * @describing addText
 	 * @param name
 	 * @author wangze
-	 * @version v1.0
 	 * @updatetime 2020-02-26
 	 */
 	addText: function(name, text) {
@@ -73,7 +91,11 @@ var pageCustom = {
 			draggable: true,
 			style: {
 				text: text ? text : ""
-			}
+			},
+			position: [100, 100]
+		});
+		this.eles[name].on("click", function(e) {
+			console.log("e", e);
 		});
 		this.zr.add(this.eles[name]);
 		console.log(this.eles[name]);
@@ -83,14 +105,13 @@ var pageCustom = {
 	 * @describing addImg
 	 * @param name
 	 * @author wangze
-	 * @version v1.0
 	 * @updatetime 2020-02-26
 	 */
 	addImg: function(name) {
 		this.eles[name] = new zrender.Image({
 			draggable: true,
 			style: {
-				image: "",
+				image: name ? name : "",
 				x: 10,
 				y: 10,
 				width: 100,
@@ -99,26 +120,49 @@ var pageCustom = {
 		})
 		this.zr.add(this.eles[name]);
 	},
-	addBgpic: function() {
-
+	/**
+	 * 添加背景图片
+	 * @describing addBgpic
+	 * @param name
+	 * @author wangze
+	 * @updatetime 2020-02-26
+	 */
+	addBgpic: function(name, src) {
+		console.log(this.zr.dom)
+		$(this.zr.dom).css({
+			backgroundImage: "url(" + src + ")",
+			backgroundSize: "100% 100%",
+			backgroundRepeat: "no-repeat",
+			backgroundPosition: "0 0"
+		})
+	},
+	/**
+	 * 删除背景图片
+	 * @describing addImg
+	 * @param name
+	 * @author wangze
+	 * @updatetime 2020-02-26
+	 */
+	delBgpic: function(name, src) {
+		$(this.zr.dom).css({
+			backgroundImage: "url()",
+		})
 	},
 	/**
 	 * 右键操作
-	 * @describing init
-	 * @param el,opts
+	 * @describing rightKeyConfig
+	 * @param el
 	 * @author wangze
-	 * @version v1.0
-	 * @updatetime 2020-02-26
+	 * @updatetime 2020-03-05
 	 */
-	rightKeyConfig: function(name) {
+	rightKeyConfig: function(el) {
 
 	},
 	/**
-	 * 确认右键弹出框的配置
+	 * 获取默认配置
 	 * @describing init
 	 * @param el,opts
 	 * @author wangze
-	 * @version v1.0
 	 * @updatetime 2020-02-26
 	 */
 	getDefaultOptions: function() {
@@ -131,4 +175,70 @@ var pageCustom = {
 			colors: ["#99CC00", "#FF99CC", "#66CC99", "#CCCC00", "#CCFF66", "#66CCCC"]
 		}
 	},
+	/**
+	 * 计算画布大小配置
+	 * @describing conNum
+	 * @param num1, num
+	 * @author wangze
+	 * @updatetime 2020-02-26
+	 */
+	conNum: function(num1, num2) {
+		if (typeof(num1) == "number" && typeof(num2) == "number") {
+			console.log(num1, num2)
+			if (num1 > 0 && num2 > 0) {
+				return true;
+			} else {
+				alert("宽高请输入大于0的有效值！");
+			}
+		} else {
+			console.log("请输入数字!", typeof(num1), typeof(num2));
+			return false;
+		}
+	},
+	/**
+	 * 阻止页面右键事件
+	 */
+	oncontextmenu: function(el) {
+		// 阻止页面右键
+		// window.oncontextmenu = function() {
+		// 	return false;
+		// };
+		// 阻止元素右键
+		el.oncontextmenu = function(e) {
+			//左键--button属性=1，右键button属性=2
+			if (e.button == 2) {
+				e.preventDefault();
+			}
+		}
+	},
+	/**
+	 * 根据配置画图
+	 * @describing customPageRender:function(opts){
+	 * @param opts
+	 * @author wangze
+	 * @updatetime 2020-02-26
+		* opts:{
+			 text:{
+					text1:{
+						//图形id
+						id:1,
+						//图形配置
+						opt:{
+							style:{},
+							shape:{}
+						}
+					}
+				},
+				imgs:{},
+				rect:{}
+		}
+	 */
+	customPageRender: function(opts) {
+		//元素个数（totle）
+
+		//元素类型（文字，图片，区域图形）
+
+		//
+
+	}
 };
